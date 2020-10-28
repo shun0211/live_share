8228d65be93f942f6afa5ab3ebb69c4e1332d778
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-10-28 18:33:46 +0900

    carrie wave使用準備完了

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,6 +8,14 @@ class TicketsController < ApplicationController
   end
 
   def create
+    @ticket = Ticket.new(ticket_params)
+    @ticket.save
   end
+
+  private
+  def ticket_params
+    params.require(:ticket).permit(:number_of_sheets, :sheet_type, :price, :shipping, :delivery_method, :description, :event_id)
+  end
+
 
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,5 @@
 class Ticket < ApplicationRecord
+  mount_uploader :thumbnail, ImageUploader
+
   belongs_to :event
 end
