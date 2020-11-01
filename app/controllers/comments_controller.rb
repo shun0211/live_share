faf814d3ff3a24e4eec09ad9d092d3666836b5bb
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-11-01 16:30:23 +0900

    チケット詳細画面コーディング完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.save!
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :ticket_id)
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :ticket
+  belongs_to :user, optional: true
+  belongs_to :ticket, optional: true
 end
