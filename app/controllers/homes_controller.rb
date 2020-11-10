a00cf1edcc0cecc9d281d444ea6b374082b8b9cd
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-11-11 07:01:57 +0900

    トップページhtml修正完了

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
-
   def index
+    @new_tickets = Ticket.order("created_at DESC").limit(8)
+    @hot_tickets = Ticket.all.limit(8).sort{|a, b| b.likes.count <=> a.likes.count}
   end
-
 end
