55bd23e0a0f4132e368a837d74aad55e739a4176
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-11-28 16:41:36 +0900

    unicorn.conf.rb修正

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,18 +1,21 @@
-# set lets
-$worker  = 2
-$timeout = 30
-$app_dir = "/var/www/rails/live_share/current"
-$listen  = File.expand_path 'shared/tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path 'shared/tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'shared/log/unicorn.log', $app_dir
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../../', __FILE__)
+
+
+# $worker  = 2
+# $timeout = 30
+# $app_dir = "/var/www/rails/live_share/current"
+# $listen  = File.expand_path 'shared/tmp/sockets/.unicorn.sock', $app_dir
+# $pid     = File.expand_path 'shared/tmp/pids/unicorn.pid', $app_dir
+# $std_log = File.expand_path 'shared/log/unicorn.log', $app_dir
 # set config
-worker_processes  $worker
-working_directory $app_dir
-stderr_path $std_log
-stdout_path $std_log
-timeout $timeout
-listen  $listen
-pid $pid
+worker_processes 2
+working_directory "#{app_path}/current"
+stderr_path "#{app_path}/shared/log/unicorn.log"
+stdout_path "#{app_path}/shared/log/unicorn.log"
+timeout 30
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 # loading booster
 preload_app true
 # before starting processes
