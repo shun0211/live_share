275213737d8c36faec5b884c18a7a31b58f366d0
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2020-11-28 17:18:09 +0900

    unicorn.conf.rb修正ver4

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -28,18 +28,26 @@ run_once = true
 before_exec do |server|
   ENV['BUNDLE_GEMFILE'] = "#{app_path}/current/Gemfile"
 end
-# before starting processes
+
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
+  end
+
   old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exist?(old_pid) && old_pid != server.pid
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
-      Process.kill "QUIT", File.read(old_pid).to_i
-    rescue Errno::ENOENT, Errno::ESRCH
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
     end
   end
 end
-# after finishing processes
-after_fork do |server, worker|
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
