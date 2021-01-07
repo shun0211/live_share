b5d845ef5de66fca04f8949a1e2b6f7cbf17b8c7
shun0211 <ss211_sabcabc12312395_18@yahoo.co.jp>
2021-01-07 11:48:22 +0900

    不要なメソッド削除

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -8,10 +8,4 @@ class MessageBroadcastJob < ApplicationJob
     # ActionCable.server.broadcast("room_channel_#{message.room_id}", { message: render_message(message) })
   end
 
-  private
-
-  # def render_message(message)
-  #   binding.pry
-  #   ApplicationController.render_with_signed_in_user(message.user, partial: 'messages/message', locals: { message: message })
-  # end
 end
